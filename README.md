50dc7608e5db9f14bf33573bf60c96f43de23671
AfiaMaham <afiamaham08@gmail.com>
2024-12-30 12:03:21 +0500

    c-file

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 
 Copy the question provided through test portal
 
+Qno1:
+     Write a program that takes an array of numbers (with both even and odd values) 
+     from the user (or declare ) and displays the sum of all even numbers from the array.
 
 copy .vscode folder from your previous example
--- c-file.c
+++ c-file.c
@@ -8,7 +8,7 @@
 extern "C" {
 #endif
 
-void __stdcall asmfunc(int p1 ,int p2);
+void __stdcall asmfunc(int* arr, int len, int* sum);
 
 #ifdef __cplusplus
 }
@@ -17,18 +17,24 @@ void __stdcall asmfunc(int p1 ,int p2);
 
 int main() {
     system("cls");
-    int abc;
-    printf("assembly proc calling from  from C! \n");
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};      // declared array of integers (having even and odd values)
+    int sum = 0;                                        // declare sum variable and initialize to 0.
+    int len = sizeof(arr) / sizeof(arr[0]);             // calculated length of array
+    printf("Array: {");
+    for(int i = 0; i < len; i++){                      // for loop to print all elements of array
+        printf("%d, ",arr[i]);
+    }
+    printf("}\nassembly proc calling from  from C! \n");
 getch();
 
     
-    asmfunc(3,5); //assembly proc calling
+    asmfunc(arr, len, &sum); //assembly proc calling
    
    getch();
     
     
     printf("back to  C! \n"); // printing in c
-    
+    printf("Sum of even are: %d",sum);         // print sum of even numbers
   
     
     return 0;
